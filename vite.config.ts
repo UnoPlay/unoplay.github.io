5bc7f9e8037a5d4a795bec05d7558a2a499933e3
rimuruyeban <eventskritniyagent@gmail.com>
2025-02-01 02:48:18 +0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    port: 3000,
-  },
+  base: '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: true,
+  },
+  server: {
+    port: 3000,
   },
 })
